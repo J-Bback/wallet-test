d081aeece89c3f94e8ec8dd96f3195a8328855f5
HyunjaePark <hyunjae@HyunjaeParkui-MacBookPro.local>
2022-01-10 00:55:14 +0900

    지갑 연동 및 초기세팅

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,14 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Wallet } from './Wallet';
+
+// Use require instead of import, and order matters
+require('@solana/wallet-adapter-react-ui/styles.css');
+require('./index.css');
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <StrictMode>
+        <Wallet />
+    </StrictMode>,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
