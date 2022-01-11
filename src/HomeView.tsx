aecb222882835f0e45cbc48c5386c937d240b700
GyuSeok <gyskjng@gmail.com>
2022-01-11 21:48:08 +0900

    feat: add transaction

--- src/HomeView.tsx
+++ src/HomeView.tsx
@@ -5,15 +5,16 @@ import * as anchor from "@project-serum/anchor";
 import { useConnection } from "@solana/wallet-adapter-react";
 import { useAnchorWallet } from "@solana/wallet-adapter-react";
 import { LAMPORTS_PER_SOL } from "@solana/web3.js";
+import { WalletNotConnectedError } from '@solana/wallet-adapter-base';
 
 export const HomeView: FC = ({}) => {
 	const wallet = useAnchorWallet();
-	const { publicKey } = useWallet();
-	const { connection } = useConnection();
-
-	const [balance, setBalance] = useState<number>();
-	const [amount, setAmount] = useState("");
-	const [sendAmount, setSendAmount] = useState("");
+	const { publicKey, sendTransaction } = useWallet();
+  const { connection } = useConnection();
+  
+  const [balance, setBalance] = useState<number>();
+  const [amount, setAmount] = useState('');
+  const [sendAmount, setSendAmount] = useState('');
 
 	const [receiver, setReceiver] = useState(
 		"3scBXUWsTyxQ9LiZSVdFu4RdmMQntt1eicMtkxbVWqbZ"
@@ -32,34 +33,35 @@ export const HomeView: FC = ({}) => {
 				}
 			}
 		})();
-	}, [wallet, connection]);
+  }, [wallet, connection]);
 
-	const setAirdrop = async () => {
-		if (publicKey) {
-			let airdropSignature = await connection.requestAirdrop(
-				publicKey,
-				Number(amount) * 10 ** 9
-			);
-			await connection.confirmTransaction(airdropSignature);
-		}
-	};
+    const setAirdrop = async () => {
+    const connection = new web3.Connection(web3.clusterApiUrl('devnet'),'confirmed');
+    if (publicKey) {
+      let airdropSignature = await connection.requestAirdrop(publicKey, Number(amount) * (10 ** 9));
+      console.log('airdropSignature', airdropSignature);
+      await connection.confirmTransaction(airdropSignature);
+      // connection.close();
+    }
+  }
 
-	const setSendSol = async () => {
-		const keyPair = anchor.web3.Keypair.generate();
-		console.log("keyPair", keyPair);
-		const receiverPubkey = new anchor.web3.PublicKey(receiver);
-		let transaction = new web3.Transaction();
+    const setSendSol = async () => {
+      const connection = new web3.Connection(web3.clusterApiUrl('devnet'),'confirmed');
 
-		// Add an instruction to execute
-		// transaction.add(web3.SystemProgram.transfer({
-		//     fromPubkey: payer.publicKey,
-		//     toPubkey: toPublicKey,
-		//     lamports: this.state.amount,
-		// }));
+      if (!publicKey) throw new WalletNotConnectedError();    
+      const transaction = new web3.Transaction().add(
+        web3.SystemProgram.transfer({
+          fromPubkey: publicKey,
+          toPubkey: new web3.PublicKey(receiver),
+          lamports: Number(sendAmount)
+        })
+      );
 
-		// await web3.sendAndConfirmTransaction(connection, transaction, [payer])
-	};
+      const signature = await sendTransaction(transaction, connection);
+      await connection.confirmTransaction(signature, 'processed');
+    };
 
+  console.log('receiverBalance', receiverBalance);
 	return (
 		<main>
 			<p>{publicKey ? <>Your address: {publicKey.toBase58()}</> : null}</p>
@@ -89,153 +91,3 @@ export const HomeView: FC = ({}) => {
 		</main>
 	);
 };
-// class Home extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       fromPublicKey: 'DPiHuhF518ZAM4zfBRzwG2iGgeB9QBY8oXjYJCpfXmZz',
-//       toPublicKey: '2VsyBLaCRSWiM3FF7vRuTTtthqn7Ne9hpWYA38DzfLzQ',
-//       fromSecretKey: '',
-//       result: '',
-//       fromBalance: null,
-//       toBalance: null,
-//       amount: 0,
-//       publicKey: this.props.publicKey,
-//     }
-//   };
-
-//   getPublicKey = () => {
-//     const { publicKey } = useWallet();
-//     console.log('publicKey', publicKey);
-//   }
-
-//   handlePublicKeyInputChange = (e) => {
-//     this.setState({ fromPublicKey: e.target.value })
-//   }
-
-//   handleAmountInputChange = (e) => {
-//     this.setState({amount: e.target.value })
-//   }
-
-//   handleSecretInputChange = (e) => {
-//     this.setState({ fromSecretKey: e.target.value })
-//   }
-
-//   handleYourPubilcInputChange = (e) => {
-//     this.setState({ toPublicKey: e.target.value })
-//   }
-
-//   handleClick = async () => {
-//     const lamports = await this.getBalance()
-//     this.setState({ fromBalance: lamports });
-//   }
-
-//   handleAirdrop = async () => {
-//     await this.setAirdrop()
-//     const lamports = await this.getBalance()
-//     this.setState({ fromBalance: lamports });
-//   }
-
-//   handleTransfer = async () => {
-//     await this.sendSolana()
-//     const myBalance = await this.getBalance()
-//     this.setState({ fromBalance: myBalance });
-//     const yourBalance = await this.getYourBalance()
-//     this.setState({ toBalance: yourBalance });
-//   }
-
-//   getBalance = async () => {
-//     const connection = new web3.Connection(web3.clusterApiUrl('devnet'),'confirmed');
-//     const publicKey = new web3.PublicKey(this.state.fromPublicKey);
-//     const account = await connection.getAccountInfo(publicKey);
-//     return account.lamports
-//   }
-
-//   getYourBalance = async () => { // 두개 못합치나
-//     const connection = new web3.Connection(web3.clusterApiUrl('devnet'),'confirmed');
-//     const publicKey = new web3.PublicKey(this.state.toPublicKey);
-//     const account = await connection.getAccountInfo(publicKey);
-//     connection.close()
-//     return account.lamports
-//   }
-
-//   setAirdrop = async () => {
-//     const connection = new web3.Connection(web3.clusterApiUrl('devnet'),'confirmed');
-//     const publicKey = new web3.PublicKey(this.state.fromPublicKey);
-//     let airdropSignature = await connection.requestAirdrop(publicKey, Number(this.state.amount));
-//     await connection.confirmTransaction(airdropSignature);
-//     // connection.close()
-//   }
-
-//   sendSolana = async () => {
-//     const connection = new web3.Connection(web3.clusterApiUrl('devnet'),'confirmed');
-//     // 여기서 실패.. 어떻게 키페어를 가져와야될까
-//     const payer = new web3.Keypair.fromSecretKey(this.state.fromSecretKey);
-//     const toPublicKey = new web3.PublicKey(this.state.toPublicKey);
-//     let transaction = new web3.Transaction();
-
-//     // Add an instruction to execute
-//     transaction.add(web3.SystemProgram.transfer({
-//         fromPubkey: payer.publicKey,
-//         toPubkey: toPublicKey,
-//         lamports: this.state.amount,
-//     }));
-
-//     await web3.sendAndConfirmTransaction(connection, transaction, [payer])
-
-//     // connection.close()
-//   }
-
-//   render() {
-//     return (
-//       <div>
-//         <div>my public key: {this.getPublicKey}</div>
-//         <div>my balance: {this.state.fromBalance}</div>
-//         <form>
-//           <input
-//             placeholder="pubilc key"
-//             value={this.state.fromPublicKey}
-//             onChange={this.handlePublicKeyInputChange}
-//           />
-//           <input
-//             type='button'
-//             onClick={this.handleClick}
-//             value='balance'
-//           />
-//         </form>
-//         <form>
-//           <input
-//             placeholder="airdrop input"
-//             value={this.state.amount}
-//             onChange={this.handleAmountInputChange}
-//           />
-//           <input
-//             type='button'
-//             onClick={this.handleAirdrop}
-//             value='airdrop'
-//           />
-//         </form>
-//         <form>
-//           <input
-//             placeholder="my secret key"
-//             value={this.state.fromSecretKey}
-//             onChange={this.handleSecretInputChange}
-//           />
-//           <input
-//             placeholder="your public key"
-//             value={this.state.toPublicKey}
-//             onChange={this.handleYourPubilcInputChange}
-//           />
-//           <input
-//             type='button'
-//             onClick={this.handleTransfer}
-//             value='transfer sol'
-//           />
-//         </form>
-//         <div>your balance: {this.state.toBalance}</div>
-//       </div>
-//     )
-//   }
-// }
-
-// export default Home;
